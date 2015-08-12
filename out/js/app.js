60a997359e977522da7646d4209e5f405d464b78
Alex <ipritoflex@yandex.ru>
2015-08-12 19:25:16 +0300

    Update

--- out/js/app.js
+++
@@ -1,8 +0,0 @@
-$traceurRuntime.options.symbols = true;
-System.registerModule("../../js/app.js", [], function(require) {
-  "use strict";
-  var __moduleName = "../../js/app.js";
-  return {};
-});
-System.get("../../js/app.js" + '');
-//# sourceMappingURL=app.js.map
