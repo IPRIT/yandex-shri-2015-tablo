8736648fa7cd8ece3c420e8792e9b34ed1702d4b
Alex <ipritoflex@yandex.ru>
2015-08-12 17:42:45 +0300

    Initial commit

---
+++ out/js/app.js
@@ -0,0 +1,8 @@
+$traceurRuntime.options.symbols = true;
+System.registerModule("../../js/app.js", [], function(require) {
+  "use strict";
+  var __moduleName = "../../js/app.js";
+  return {};
+});
+System.get("../../js/app.js" + '');
+//# sourceMappingURL=app.js.map
